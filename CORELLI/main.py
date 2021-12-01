33d2f3df19dc6655f3477e5168bcb246ba07ebc4
lcorelli <leonardcorelli@gmail.com>
2021-12-01 15:20:53 -0500

    added estimated q scorer function

--- CORELLI/main.py
+++ CORELLI/main.py
@@ -1,8 +1,9 @@
-import d3rlpy
 from d3rlpy.datasets import get_cartpole
 from d3rlpy.algos import DiscreteCQL
-from sklearn.model_selection import train_test_split
 from d3rlpy.metrics.scorer import evaluate_on_environment
+from d3rlpy.metrics.scorer import initial_state_value_estimation_scorer
+from sklearn.model_selection import train_test_split
+
 
 dataset, env = get_cartpole() 
 cql = DiscreteCQL(use_gpu = True)    # instantiate discrete CQL algrithm 
@@ -10,5 +11,5 @@ cql = DiscreteCQL(use_gpu = True)    # instantiate discrete CQL algrithm
 train_episodes, test_episodes = train_test_split(dataset, test_size = 0.2)
 
 #cql.fit(dataset, n_steps = 10000)   # train (offline)
-cql.fit(train_episodes, eval_episodes=test_episodes, n_steps = 10000, scorers = {'environment': evaluate_on_environment(env)})
+cql.fit(train_episodes, eval_episodes=test_episodes, n_steps = 10000, scorers = {'average_reward': evaluate_on_environment(env),'estimated_q_values':initial_state_value_estimation_scorer})
 # actions = cql.predict(x)    # generate actions

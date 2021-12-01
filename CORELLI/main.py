eafa0e20c29e72fedab712f002ef0d95365fd369
lcorelli <leonardcorelli@gmail.com>
2021-12-01 14:15:40 -0500

    updated file

--- CORELLI/main.py
+++ CORELLI/main.py
@@ -1,9 +1,14 @@
 import d3rlpy
 from d3rlpy.datasets import get_cartpole
+from d3rlpy.algos import DiscreteCQL
+from sklearn.model_selection import train_test_split
 
-dataset, env = get_cartpole()
+dataset, env = get_cartpole() 
+cql = DiscreteCQL(use_gpu = True)    # instantiate discrete CQL algrithm 
 
-cql = d3rlpy.algos.DiscreteCQL(use_gpu = True)    # instantiate discrete CQL algrithm 
-cql.fit(dataset, n_steps = 10000)   # train (offline)
+train_episodes, test_episodes = train_test_split(dataset, test_size = 0.2)
+
+#cql.fit(dataset, n_steps = 10000)   # train (offline)
+cql.fit(train_episodes, eval_episodes=test_episodes, n_epochs=1, n_steps = 10000)
 # actions = cql.predict(x)    # generate actions
 

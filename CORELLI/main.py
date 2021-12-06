0b73790d48b656cfb91b9099e53259bc1d9b7c18
lcorelli <leonardcorelli@gmail.com>
2021-12-06 18:48:34 -0500

    10 epochs

--- CORELLI/main.py
+++ CORELLI/main.py
@@ -15,7 +15,7 @@ train_episodes, test_episodes = train_test_split(dataset, test_size=0.2)
 cql.fit(
     train_episodes,
     eval_episodes=test_episodes,
-    n_epochs=5,
+    n_epochs=10,
     scorers={
         'average_reward': evaluate_on_environment(env),
         'estimated_q_values': initial_state_value_estimation_scorer,
@@ -27,7 +27,7 @@ fqe = DiscreteFQE(algo=cql, use_gpu=True)
 fqe.fit(
     train_episodes,
     eval_episodes=test_episodes,
-    n_epochs=5,
+    n_epochs=10,
     scorers={
         'estimated_q_values': initial_state_value_estimation_scorer,
         'soft_opc': soft_opc_scorer(70),

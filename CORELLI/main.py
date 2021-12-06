15d2898e46824c9cab6f2d64893bcc5da875d056
lcorelli <leonardcorelli@gmail.com>
2021-12-06 12:32:53 -0500

    fixed typo

--- CORELLI/main.py
+++ CORELLI/main.py
@@ -15,4 +15,4 @@ train_episodes, test_episodes = train_test_split(dataset, test_size = 0.2)
 cql.fit(train_episodes, eval_episodes=test_episodes, n_epochs = 5, scorers = {'average_reward': evaluate_on_environment(env),'estimated_q_values':initial_state_value_estimation_scorer,'true_q_values':true_q_scorer}, with_timestamp=False, experiment_name = 'DiscreteCQL_v0')
 
 fqe = DiscreteFQE(algo = cql,use_gpu = True)
-fqe.fit(train_episodes,eval_episodes=test_episodes, on_epochs = 5, scorers={'estimated_q_values': initial_state_value_estimation_scorer,'soft_opc': soft_opc_scorer(70)}, with_timestamp=False, experiment_name = 'DiscreteFQE_v0')
+fqe.fit(train_episodes,eval_episodes=test_episodes, n_epochs = 5, scorers={'estimated_q_values': initial_state_value_estimation_scorer,'soft_opc': soft_opc_scorer(70)}, with_timestamp=False, experiment_name = 'DiscreteFQE_v0')

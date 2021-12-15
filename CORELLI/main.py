27c34082b783621eeee535b84a57a294cdc02086
lcorelli <leonardcorelli@gmail.com>
2021-12-15 15:27:33 -0500

    changed dataset to atari

--- CORELLI/main.py
+++ CORELLI/main.py
@@ -1,4 +1,4 @@
-from d3rlpy.datasets import get_cartpole
+from d3rlpy.datasets import get_atari
 from d3rlpy.algos import DiscreteCQL
 from d3rlpy.ope import DiscreteFQE
 from d3rlpy.metrics.scorer import evaluate_on_environment
@@ -7,11 +7,11 @@ from d3rlpy.metrics.scorer import soft_opc_scorer
 from d3rlpy.metrics.scorer import true_q_scorer
 from sklearn.model_selection import train_test_split
 
-
-dataset, env = get_cartpole()
-cql = DiscreteCQL(use_gpu=True)    # instantiate discrete CQL algrithm
+dataset, env = get_atari()
+cql = DiscreteCQL(use_gpu=True, n_frames = 4, scaler = 'pixel')    # instantiate discrete CQL algrithm
 
 train_episodes, test_episodes = train_test_split(dataset, test_size=0.2)
+
 cql.fit(
     train_episodes,
     eval_episodes=test_episodes,
@@ -23,6 +23,7 @@ cql.fit(
     with_timestamp=False,
     experiment_name='DiscreteCQL_v0')
 
+'''
 fqe = DiscreteFQE(algo=cql, use_gpu=True)
 fqe.fit(
     train_episodes,
@@ -34,3 +35,4 @@ fqe.fit(
         'true_q_values': true_q_scorer},
     with_timestamp=False,
     experiment_name='DiscreteFQE_v0')
+'''
